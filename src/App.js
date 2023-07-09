134f72c325ed3318745fab48a1db4c99a86c37b2
Mohit132 <mohitdubey1322001@gmil.com>
2023-07-09 13:00:29 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Columns from './components/Columns';
 import './App.css';
 import Columns from './components/Columns';
-import bg from './components/bg.jpeg'
+// import bg from './components/bg.jpeg'
 function App() {
   return (<>
-    <div className='AppHeader' style={{backgroundImage:new (URL(bg))}}>  <h3>Kanban App</h3>
+    <div className='AppHeader' style={{backgroundImage:`url("https://blog.openreplay.com/images/zustand-state-management-for-react/images/hero.png")` }}>  <h3>Kanban App</h3>
     <h5>using Zustand state management library</h5>
     </div>
     <div className="App" >
